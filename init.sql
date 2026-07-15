31fb0d48b83181884656d3563313add29c3bea6e
Lodusz <anfisin1@gmail.com>
2026-07-15 16:09:56 +0500

    some fix init.sql

--- init.sql
+++ init.sql
@@ -10,3 +10,10 @@ CREATE TABLE IF NOT EXISTS messages (
     content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO users (username) VALUES ('Тимур'), ('Юра'), ('Элина'), ('Камиль');
+INSERT INTO messages (user_id, content) VALUES 
+(1, 'Всем привет! Тестируем новый чат?'),
+(2, 'Да, вроде работает.'),
+(3, 'Ага, сообщения теперь сохраняются'),
+(4, 'Отлично, щас тоже зайду с ноута.');
