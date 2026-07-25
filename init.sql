3f5b0c5858ca996adcb5d0ad39e4dc7e36e89595
Максим Миндавлетов <maksimmindavletov@Noutbuk-Maksim.local>
2026-07-25 06:55:53 +0500

    UPDATE: rework init.sql

--- init.sql
+++ init.sql
@@ -1,19 +1,25 @@
+-- Auth Service
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
+--Chat Service 
 CREATE TABLE IF NOT EXISTS messages (
     id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id),
+    username VARCHAR(50) NOT NULL,
     content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO users (username) VALUES ('Тимур'), ('Юра'), ('Элина'), ('Камиль');
-INSERT INTO messages (user_id, content) VALUES 
-(1, 'Всем привет! Тестируем новый чат?'),
-(2, 'Да, вроде работает.'),
-(3, 'Ага, сообщения теперь сохраняются'),
-(4, 'Отлично, щас тоже зайду с ноута.');
+-- INDEX
+CREATE INDEX IF NOT EXISTS idx_messages_id_asc ON messages(id ASC);
+
+-- Тестовые данные для стартовой истории
+INSERT INTO users (username) VALUES ('Тимур'), ('Юра')
+ON CONFLICT (username) DO NOTHING;
+
+INSERT INTO messages (username, content) VALUES 
+('Тимур', 'Всем привет! Тестим мессенджер Максика'),
+('Юра', 'Ку! Всё работает чётко');
