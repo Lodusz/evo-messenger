63bd12c882ae87837d82b6fd338e38b371ca539f
Lodusz <anfisin1@gmail.com>
2026-07-31 15:37:42 +0500

    fix sql , add password

--- init.sql
+++ init.sql
@@ -1,11 +1,12 @@
--- Auth Service
+-- auth 
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
---Chat Service 
+-- chat 
 CREATE TABLE IF NOT EXISTS messages (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
@@ -13,13 +14,14 @@ CREATE TABLE IF NOT EXISTS messages (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
--- INDEX
+
 CREATE INDEX IF NOT EXISTS idx_messages_id_asc ON messages(id ASC);
 
--- Тестовые данные для стартовой истории
-INSERT INTO users (username) VALUES ('Тимур'), ('Юра')
+INSERT INTO users (username, password_hash) VALUES 
+('Тимур', 'fake_hash_1'), 
+('Юра', 'fake_hash_2')
 ON CONFLICT (username) DO NOTHING;
 
-INSERT INTO messages (username, content) VALUES 
+INSERT INTO messages (username, content) VALUES
 ('Тимур', 'Всем привет! Тестим мессенджер Максика'),
 ('Юра', 'Ку! Всё работает чётко');
