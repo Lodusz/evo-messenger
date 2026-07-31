d79b993252d71e734972212168fba628bbe13ae0
Lodusz <anfisin1@gmail.com>
2026-07-31 15:05:30 +0500

    fix server

--- server.go
+++ server.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
 
-	FRONTEND := http.FileServer(http.Dir("ДОБАВИТЬ СЮДА ПАПКУ ГДЕ ЛЕЖИТ FRONTEND"))
+	FRONTEND := http.FileServer(http.Dir(http.Dir("./frontend"))
 	http.Handle("/", fs)
 
-	log.Println("Сервер фронтенда запущен на http://localhost:3000")
+	log.Println("Сервер фронтенда запущен на :3000)
 	if err := http.ListenAndServe(":3000", nil); err != nil {
 		log.Fatal(err)
 	}
